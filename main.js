6a5199906f1a8809623443ce76f0b6e07a566d48
CeciCode <101422795+CeciCode@users.noreply.github.com>
2022-12-07 15:01:54 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,15 +1,35 @@
 song1= "";
 song2= "";
+leftWristY= "";
+leftWristX= "";
+rightWristY= "";
+rightWristX= "";
 function preload() {
-     song1= loadSound("");
-     song2= loadSound("");
+     song1= loadSound("echo.mp3");
+     song2= loadSound("jackpot sad girl.mp3");
 }
 function setup() {
      canvas= createCanvas(600, 500);
      canvas.center();
      video= createCapture(VIDEO);
      video.hide();
+     poseNet= ml5.poseNet(video, modelLoaded);
+     poseNet.on("pose", gotPoses);
 }
+ function modelLoaded() {
+     console.log("pose net 👍");
+}
+function gotPoses(results) {
+     if(results.length > 0) {
+         console.log(results);
+         rightWristX= results[0].pose.rightWrist.x;
+         rightWristY= results[0].pose.rightWrist.y;
+         console.log("rightWristX= " + rightWristX + "rightWristY= " + rightWristY);
+         leftWristX= results[0].pose.leftWrist.x;
+         leftWristY= results[0].pose.leftWrist.y;
+         console.log("leftWristX= " + leftWristX + "leftWristY= " + leftWristY);
+     }
+ }
 function draw() {
      image(video, 0, 0, 600, 500);
 }
